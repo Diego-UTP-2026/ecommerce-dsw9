9035aff9afb5fbe8137253d8b33f1522e765e7d7
Diego-UTP-2026 <diegoluis.cordoba@utp.ac.pa>
2026-05-23 20:27:18 +0000

    feat: Prueba de Modulo Admin

--- models/Order.js
+++ models/Order.js
@@ -1,4 +1,5 @@
 // models/Order.js
+// models/Order.js
 const { DataTypes } = require('sequelize');
 const sequelize     = require('../config/database');
 
@@ -24,4 +25,37 @@ const Order = sequelize.define('Order', {
   }
 });
 
+// ── AGREGAR ASOCIACIONES EXPLÍCITAS ────────────────────────────
+// Esto le dice a Sequelize que use 'user_id' al hacer Joins/Includes
+Order.associate = (models) => {
+  Order.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+};
+
 module.exports = Order;
+
+/*const { DataTypes } = require('sequelize');
+const sequelize     = require('../config/database');
+
+const Order = sequelize.define('Order', {
+  id:        { type: DataTypes.INTEGER,        primaryKey: true, autoIncrement: true },
+  firstName: { type: DataTypes.STRING,         allowNull: false },
+  lastName:  { type: DataTypes.STRING,         allowNull: false },
+  email:     { type: DataTypes.STRING,         allowNull: false },
+  address:   { type: DataTypes.STRING,         allowNull: false },
+  city:      { type: DataTypes.STRING,         allowNull: false },
+  province:  { type: DataTypes.STRING,         allowNull: false },
+  zip:       { type: DataTypes.STRING },
+  phone:     { type: DataTypes.STRING,         allowNull: false },
+  total:     { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+  paymentId: { type: DataTypes.STRING },
+  // pending | paid | payment_failed | canceled
+  status:    { type: DataTypes.STRING,         defaultValue: 'pending' },
+
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: { model: 'users', key: 'id' }
+  }
+});
+
+module.exports = Order;*/
